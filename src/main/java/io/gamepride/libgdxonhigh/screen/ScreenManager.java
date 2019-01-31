c4516bf9f7fe7cc27e771d6913b854a79ff758ab
mkjio <16marchello96@gmail.com>
2019-01-31 17:47:31 +0300

    ScreenManager implemented

---
+++ src/main/java/io/gamepride/libgdxonhigh/screen/ScreenManager.java
@@ -0,0 +1,41 @@
+package io.gamepride.libgdxonhigh.screen;
+
+import com.badlogic.gdx.Screen;
+import io.gamepride.libgdxonhigh.Root;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Marat Kadzhaev
+ * @since 0.1.0
+ */
+public class ScreenManager {
+
+    private final Root root;
+    private final Map<ScreenType, Screen> screens = new HashMap<>();
+
+    public ScreenManager(final Root root) {
+        this.root = root;
+    }
+
+    public void changeScreen(ScreenType screenType, Object...args) {
+        Screen screen = screens.get(screenType);
+        if (screen == null) {
+            try {
+                screen = screenType.getClazz().getDeclaredConstructor(Root.class, Object[].class).newInstance(root, args);
+                screens.put(screenType, screen);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        root.setScreen(screen);
+    }
+
+}
---
+++ src/main/java/io/gamepride/libgdxonhigh/screen/ScreenType.java
@@ -0,0 +1,45 @@
+package io.gamepride.libgdxonhigh.screen;
+
+import com.badlogic.gdx.Screen;
+import java.util.Objects;
+
+/**
+ * @author Marat Kadzhaev
+ * @since 0.1.0
+ */
+public class ScreenType {
+
+    private String name;
+    private Class<Screen> clazz;
+
+    public ScreenType(String name, Class<Screen> clazz) {
+        this.name = name;
+        this.clazz = clazz;
+    }
+
+    public Class<Screen> getClazz() {
+        return clazz;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ScreenType that = (ScreenType) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}

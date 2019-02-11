34f36bcdcba4991f93363d7e39ced02fea983d2d
mkjio <16marchello96@gmail.com>
2019-02-11 14:08:42 +0300

    Added Assert utility class.

---
+++ src/main/java/io/gamepride/libgdxonhigh/util/Assert.java
@@ -0,0 +1,24 @@
+package io.gamepride.libgdxonhigh.util;
+
+/**
+ * Assertion utility class that assists in validating arguments.
+ * Useful for identifying programmer errors early and clearly at runtime.
+ * This class is similar to JUnit's assertion library. If an argument value is
+ * deemed invalid, an {@link IllegalArgumentException} is thrown (typically).
+ *
+ * @author Marat Kadzhaev
+ * @since 0.1.0
+ */
+public abstract class Assert {
+
+    public static void notNull(Object object) {
+        notNull(object, "[Assertion failed] - this argument is required; it must not be null");
+    }
+
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+}

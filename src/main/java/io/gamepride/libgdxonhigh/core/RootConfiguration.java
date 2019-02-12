7ca90f49d97f78d5a39b84f66d3f651a0533face
mkjio <16marchello96@gmail.com>
2019-02-12 13:49:26 +0300

    Added required field for RootConfiguration builder.

--- src/main/java/io/gamepride/libgdxonhigh/core/RootConfiguration.java
+++ src/main/java/io/gamepride/libgdxonhigh/core/RootConfiguration.java
@@ -1,8 +1,8 @@
 package io.gamepride.libgdxonhigh.core;
 
+import com.badlogic.gdx.graphics.Camera;
 import io.gamepride.libgdxonhigh.integration.Platform;
 import io.gamepride.libgdxonhigh.screen.ScreenType;
-import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -10,13 +10,70 @@ import lombok.Data;
  * @since 0.1.0
  */
 @Data
-@Builder
 public final class RootConfiguration {
 
     private Platform platform;
     private String preferencesName;
     private String i18nBundlePath;
     private ScreenType autoOpenScreen;
+    private Class<? extends Camera> camera;
     private int applicationWidth, applicationHeight;
+
+    public static Builder builder(int applicationWidth, int applicationHeight) {
+        return new Builder(applicationWidth, applicationHeight);
+    }
+
+    public static class Builder {
+
+        private final int applicationWidth, applicationHeight;
+        private Platform platform;
+        private String preferencesName;
+        private String i18nBundlePath;
+        private ScreenType autoOpenScreen;
+        private Class<? extends Camera> camera;
+
+        public Builder(int applicationWidth, int applicationHeight) {
+            this.applicationWidth = applicationWidth;
+            this.applicationHeight = applicationHeight;
+        }
+
+        public Builder platform(Platform platform) {
+            this.platform = platform;
+            return this;
+        }
+
+        public Builder preferencesName(String preferencesName) {
+            this.preferencesName = preferencesName;
+            return this;
+        }
+
+        public Builder i18nBundlePath(String i18nBundlePath) {
+            this.i18nBundlePath = i18nBundlePath;
+            return this;
+        }
+
+        public Builder autoOpenScreen(ScreenType screenType) {
+            this.autoOpenScreen = screenType;
+            return this;
+        }
+
+        public Builder camera(Class<? extends Camera> camera) {
+            this.camera = camera;
+            return this;
+        }
+
+        public RootConfiguration build() {
+            RootConfiguration configuration = new RootConfiguration();
+            configuration.setApplicationHeight(applicationHeight);
+            configuration.setApplicationWidth(applicationWidth);
+            configuration.setCamera(camera);
+            configuration.setI18nBundlePath(i18nBundlePath);
+            configuration.setAutoOpenScreen(autoOpenScreen);
+            configuration.setPreferencesName(preferencesName);
+            configuration.setPlatform(platform);
+            return configuration;
+        }
+
+    }
 
 }
